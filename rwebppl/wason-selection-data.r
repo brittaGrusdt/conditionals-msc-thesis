877776724c58752d3924ddcc65a71ff44978680a
bgrusdt <grusdt@uni-potsdam.de>
2018-10-05 11:03:23 +0200

    updated wason selection script for PL with mcmc, added expectedVals PL

--- rwebppl/wason-selection-data.r
+++ rwebppl/wason-selection-data.r
@@ -1,27 +1,28 @@
 #### RUN once
-EV_LL <- data.frame(matrix(ncol = 4, nrow = 12))
-colnames(EV_LL) <- c("ac", "nac", "anc", "nanc")
-rownames(EV_LL) <- c("a", "c", "a-c", "c-a",
+EV_listener <- data.frame(matrix(ncol = 4, nrow = 12))
+colnames(EV_listener) <- c("ac", "nac", "anc", "nanc")
+rownames(EV_listener) <- c("a", "c", "a-c", "c-a",
                      "na", "nc",
                      "na-c", "a-nc", "na-nc",
                      "nc-a", "c-na", "nc-na")
 
-model_path <- "/home/britta/UNI/Masterarbeit/RwebPPL/webppl/literalListener.wppl"
+#model_path <- "/home/britta/UNI/Masterarbeit/RwebPPL/webppl/literalListener.wppl"
+model_path <- "/home/britta/UNI/Masterarbeit/RwebPPL/webppl/listener.wppl"
 
 posterior <- function(){
-  LL <- webppl(program_file = model_path)
-  return(LL)
+  listener <- webppl(program_file = model_path)
+  return(listener)
 }
 
 ####
-getTableEntries <- function(LL){
-  df <- data.frame(matrix(ncol=4, nrow=nrow(LL)))
+getTableEntries <- function(jointPs){
+  df <- data.frame(matrix(ncol=4, nrow=length(jointPs)))
   colnames(df) <- c("ac", "nac", "anc", "nanc")
 
-  df$ac <- as.numeric(lapply(LL$jointP, function(l) l[[1]]))
-  df$nac <- as.numeric(lapply(LL$jointP, function(l) l[[2]]))
-  df$anc <- as.numeric(lapply(LL$jointP, function(l) l[[3]]))
-  df$nanc <- as.numeric(lapply(LL$jointP, function(l) l[[4]]))
+  df$ac <- as.numeric(lapply(jointPs, function(l) l[[1]]))
+  df$nac <- as.numeric(lapply(jointPs, function(l) l[[2]]))
+  df$anc <- as.numeric(lapply(jointPs, function(l) l[[3]]))
+  df$nanc <- as.numeric(lapply(jointPs, function(l) l[[4]]))
 
   return(df)
 }
@@ -37,18 +38,30 @@ computeEVs <- function(entries, probs){
   return(df)
 }
 
-getEVs <- function(){
-  LL <- posterior()
-  tableEntries <- getTableEntries(LL)
-  EVs <- computeEVs(tableEntries, LL$prob)
+getEVs <- function(filename, mcmc){
+  listener <- posterior()
+  saveRDS(listener, filename)
+  if(mcmc==TRUE){
+    tables <- listener$value[listener$Parameter=="jointP"]
+    tableEntries <- getTableEntries(tables)
+    EVs <- computeEVs(tableEntries, rep(1/length(tables), length(tables)))
+  }else{
+    tableEntries <- getTableEntries(listener$jointP)
+    EVs <- computeEVs(tableEntries, listener$prob)
+  }
   return(EVs)
 }
 
 #### RUN for each utterance
-utterance <- "nc-na"
-EVs <- getEVs()
+mcmc <- TRUE
+utterance <- "a"
+filename <- paste("PL-mcmc-g20-utt-", utterance , ".rds", sep="")
+EVs <- getEVs(filename, mcmc)
 
-EV_LL[utterance, "ac"] = EVs$ac
-EV_LL[utterance, "nac"] = EVs$nac
-EV_LL[utterance, "anc"] = EVs$anc
-EV_LL[utterance, "nanc"] = EVs$nanc
+EV_listener[utterance, "ac"] = EVs$ac
+EV_listener[utterance, "nac"] = EVs$nac
+EV_listener[utterance, "anc"] = EVs$anc
+EV_listener[utterance, "nanc"] = EVs$nanc
+
+####
+saveRDS(EV_listener, "expectedValsPL.rds")
