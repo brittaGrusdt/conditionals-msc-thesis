07be0e49ddf7d353c5b455c3dbb4f52585855a1f
bgrusdt <grusdt@uni-potsdam.de>
2018-11-30 09:56:20 +0100

    added random seed

--- rwebppl/running-functions.r
+++ rwebppl/running-functions.r
@@ -2,24 +2,38 @@ require("rwebppl")
 source("data-processing-functions.r")
 ##########################################
 
-posterior <- function(model_path){
-  listener <- webppl(program_file = model_path)
+getSeed <- function(){
+  time <- as.numeric(Sys.time())
+  return(time)
+}
+
+posterior <- function(model_path, seed=FALSE){
+  if(!seed){
+    seed <- getSeed()
+  }
+  listener <- webppl(program_file = model_path, random_seed=seed)
   return(listener)
 }
-posterior_with_data_input <- function(model_path, data){
-  listener <- webppl(program_file = model_path, data=data, data_var='myDF')
+posterior_with_data_input <- function(model_path, data, viz, seed=FALSE){
+  if(!seed){
+    seed <- getSeed()
+  }
+  if(viz){
+    webppl(program_file=model_path, data=data, data_var='myDF',
+                       packages=c("webppl-viz"), random_seed = seed)
+    listener <- ''
+  }
+  listener <- webppl(program_file = model_path, data=data, data_var='myDF', random_seed=seed)
   return(listener)
 }
 
-eval <- function(listener, listenerType, inferType){
-  if(inferType == "samples"){
-    df <- buildDF_from_samples(listener)
-    ps <- rep(1/length(df$jointP), length(df$jointP))
-  }else{
-    df <- buildDF_from_enumerate(listener)
-    ps <- listener$prob
-  }
-  probs <- computeProbs(df)
-  EVs <- computeEVs(probs, ps, listenerType)
+getEVs <- function(listener_df, listenerType){
+
+  EV_probs <- marginalEVTables(listener_df, listenerType)
+  EV_quds <- marginalEVQuds(listener_df)
+  EV_cns <- marginalEVCNs(listener_df)
+  EVs <- cbind(EV_quds,EV_probs, EV_cns)
   return(EVs)
 }
+
+
