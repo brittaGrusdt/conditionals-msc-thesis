a4983f0de2861408a9cf52505fc088e2f28d1e7a
bgrusdt <grusdt@uni-potsdam.de>
2019-01-09 14:47:37 +0100

    added constants for biases, basedir

--- rwebppl/constants.r
+++ rwebppl/constants.r
@@ -2,5 +2,6 @@ CNs <- c('A implies C', 'A implies -C', '-A implies C', '-A implies -C',
          'C implies A', 'C implies -A', '-C implies A','-C implies -A', 
          'A ind. C')
 QUDs <- c("bn", "table")
-BASEDIR <- "/home/britta/UNI/Masterarbeit/conditionals/model/listener/"
+BASEDIR <- "/home/britta/UNI/Masterarbeit/conditionals/model/src/"
+BIASES <- c("none", "lawn", "pizza", "douven1")
 SEEDS <- c(8546340, 6538185, 5230322)
