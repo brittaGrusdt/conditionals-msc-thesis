174458b9d46cf81bdb8dd653b3af223ccc771b5f
bgrusdt <grusdt@uni-potsdam.de>
2019-01-31 10:25:11 +0100

    added utterance sorted by costs to constants

--- rwebppl/constants.r
+++ rwebppl/constants.r
@@ -5,3 +5,9 @@ QUDs <- c("bn", "table")
 BASEDIR <- "/home/britta/UNI/Masterarbeit/conditionals/model/src/"
 BIASES <- c("none", "lawn", "pizza", "douven1")
 SEEDS <- c(8546340, 6538185, 5230322)
+
+UTT_ORDER_BY_COST <- c("A", "C", "likely A", "likely C", "-A", "-C",
+                       "likely -A", "likely -C", "A and C", "C but -A",
+                       "A but -C", "neither A nor C", "If A, C",
+                       "If C, A", "If -A, C", "If A, -C", "If -C, A",
+                       "If C, -A", "If -A, -C", "If -C, -A")
